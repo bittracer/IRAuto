3e1e194fd737ef5ef35725a45115eb7c83b9a46e
mayur gauswami <gosaimayur91@yahoo.com>
2015-04-06 15:33:35 +0530

    SETTING SCREEN UPDATED
    setting screen is completed and changed constant
    .h

--- IRAutomation/IRSettingViewController.m
+++ IRAutomation/IRSettingViewController.m
@@ -20,6 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+        [self initialize];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,5 +41,41 @@
     [appDel.controller setCenterViewController:rootView withCloseAnimation:YES  completion:^(BOOL finished) {
         
     }];
+}
+
+
+-(void) initialize{
+    
+    // Array of Images
+    imgList = @[@"notification_icn",@"celcius_icn"];
+    
+    //Array of TextField
+    nameList = @[@"notification",@"Farenheit/Celcius"];
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [nameList count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = SETTING_CELL_IDENTYFIER;
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    UILabel *lblTitle = (UILabel *)[cell.contentView viewWithTag:SETTING_CELL];
+    
+    lblTitle.text = [nameList objectAtIndex:indexPath.row];
+  
+    
+    cell.imageView.image=[UIImage imageNamed:[imgList objectAtIndex:indexPath.row]];
+    
+    //just removes the extra lines
+    tableView.tableFooterView = [UIView new];
+    
+    return cell;
 }
 @end
